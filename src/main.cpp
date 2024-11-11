8cb26e33529f4b7f3a5a6bf0d75246804460b267
sefyan0hack <sefyanhack@gmail.com>
2024-11-11 23:07:37 +0100

    Fix Goodmatch , add border to refrech button , open url on  win

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,22 @@
 #include <cmath>
 #include <random>
 #include <string>
+#include <tuple>
 
 #include "Res.hpp"
 
+#define FPS 30
 #define padding 5
 
 struct Card
 {
     Texture2D t;
     int id;
+    bool active;
 };
 
 int main(){
+    
     const char* GameName = "Hello, Raylib";
 
     std::random_device rd;
@@ -25,8 +29,10 @@ int main(){
     int screenWidth = 660;
     int screenHeight = 660;
 
+    SetConfigFlags(FLAG_MSAA_4X_HINT);
+
     InitWindow(screenWidth, screenHeight, "Game");
-    SetTargetFPS(60);
+    SetTargetFPS(FPS);
     InitResouces();
 
     int TitleFontSize = 30;
@@ -58,7 +64,7 @@ int main(){
     };
 
     std::vector<Texture2D> texs;
-
+    
     for (const auto& img : imgsNames)
     {   
         texs.push_back(LoadTextureWithSize(img, width_gab/5, height_gab/5));
@@ -70,14 +76,17 @@ int main(){
     {
         for (size_t j = 0; j < 2; j++)
         {
-            map.push_back({texs[i], (int)i});
+            map.push_back( Card {texs[i], (int)i, false} );
         }
     }
-    
-    std::shuffle(map.begin(), map.end(), rng);
+    //sh
+    for(unsigned char i = 0; i < (unsigned char)5; i++){
+        std::shuffle(map.begin(), map.end(), rng);
+    }
 
-    std::vector<std::pair<int, Vector2>> GoodMatsh;
-    
+    //                     id   position active
+    std::vector<std::tuple<int, Vector2, bool*>> GoodMatsh;
+    std::string inf = "";
     while (!WindowShouldClose())
     {
         if (IsWindowResized() && !IsWindowFullscreen())
@@ -95,8 +104,13 @@ int main(){
             
             auto refAssetInfo = GetResInfo(REFRECH);
             Rectangle refechBRec = { (float)(screenWidth - (float)refAssetInfo.w)/2, screenHeight -  (float)(screenHeight - height_gab)/2 - (float)refAssetInfo.h/2 , (float)refAssetInfo.w, (float)refAssetInfo.h};
-            // DrawRectangleRec(refechBRec, WHITE);
-            DrawTexture(Assets[REFRECH], refechBRec.x, refechBRec.y, BLACK);
+            
+            if(CheckCollisionPointRec(GetMousePosition(), refechBRec)){
+                DrawTexture(Assets[REFRECH], refechBRec.x, refechBRec.y, Fade(BLACK, 0.9f));
+                DrawTexture(Assets[REFRECH], refechBRec.x, refechBRec.y, Fade(BLACK, 0.9f));
+            }else{
+                DrawTexture(Assets[REFRECH], refechBRec.x, refechBRec.y, BLACK);
+            }
 
             for (size_t i = 0; i < dim; i++)
             {
@@ -105,7 +119,7 @@ int main(){
                     int x = rectx + i * (width_gab /5 + gab);
                     int y = recty + j * (height_gab /5 + gab);
                     
-                    auto card = map[i * (dim-1) + j];
+                    auto& card = map[i * (dim-1) + j];
 
                     curr = {(float)x, (float)y, (float)width_gab/5, (float)height_gab/5 };
 
@@ -113,38 +127,50 @@ int main(){
                     DrawRectangleRounded(curr, 0.1f, 60, { 102, 191, 255, 155});
                     // DrawTexture(map[i * (dim-1) + j].t, x, y, WHITE);
 
-                    if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
-                        
+                    auto mousePos = GetMousePosition();
+                    // if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+                    if(IsMouseButtonDown(MOUSE_BUTTON_LEFT)){
                         //check if i pressed on the card to push it
-                        auto mousePos = GetMousePosition();
                         if(CheckCollisionPointRec(mousePos, curr)){
-                            GoodMatsh.push_back(std::make_pair(card.id, CLITERAL(Vector2){(float)x, (float)y}));
+                            if ( not card.active)
+                            {
+                                GoodMatsh.push_back( std::make_tuple( card.id, Vector2 {(float)x, (float)y}, &card.active ) );
+                            }
                         }
 
                         //check if i pressed on the Replay Button to Replay 
                         if(CheckCollisionPointRec(mousePos, refechBRec)){
+                            //suffle 2 times :)
                             std::shuffle(map.begin(), map.end(), rng);
+                            std::shuffle(map.begin(), map.end(), rng);
+                            for (auto &c : map)
+                            {
+                                c.active = false;
+                            }
                             GoodMatsh.clear();
                         }
                     }
-
+                    
                     //drwa only Good Matshed imgs
                     for (const auto &mcard : GoodMatsh)
                     {
-                        DrawTexture(texs[mcard.first], mcard.second.x, mcard.second.y, WHITE);
+                        const auto& [id, pos, _] = mcard;
+                        DrawTexture(texs[id], pos.x, pos.y, WHITE);
                     }
 
                     //check if 2 cards are the same
                     if(not GoodMatsh.empty() && GoodMatsh.size() >= 2){
                         
                         if(GoodMatsh.size() % 2 == 0){
-                            auto t1 = GoodMatsh.end() - 1;
-                            auto t2 = GoodMatsh.end() - 2;
+                            auto& [t1_id, t1_pos, t1_active] = *(GoodMatsh.end() - 1);
+                            auto& [t2_id, t2_pos, t2_active] = *(GoodMatsh.end() - 2);
 
-                            if(t1->first == t2->first){
-                                if(t1->second.x == t2->second.x && t1->second.y == t2->second.y){
+                            if(t1_id == t2_id){
+                                if(t1_pos.x == t2_pos.x && t1_pos.y == t2_pos.y){
                                     GoodMatsh.pop_back();
                                 }else{
+                                    *t1_active = true;
+                                    *t2_active = true;
                                     continue;
                                 }
                             }else{
@@ -159,6 +185,15 @@ int main(){
             
 
             DrawText("Matching Card!", textX, padding, TitleFontSize, GOLD);
+
+            inf += std::to_string(GoodMatsh.size());
+            DrawText(inf.c_str(), padding, padding, TitleFontSize, GOLD);
+            inf.clear();
+
+            if(GoodMatsh.size() == map.size()){
+                OpenURL("https://youtube.com");
+                exit(1);
+            }
             
         EndDrawing();
     }
