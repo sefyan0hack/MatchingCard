fe03b1d7cc8be048ec5b3efdcbf448aaf9c99675
sefyan0hack <sefyanhack@gmail.com>
2024-11-14 00:55:59 +0100

    imporoving build.sh for resources

--- res/buildres.sh
+++ res/buildres.sh
@@ -1,6 +1,7 @@
-> all_res.hpp
+# > all_res.hpp
+# echo "#pragma once" >> all_res.hpp;
 
-echo "#pragma once" >> all_res.hpp;
+FinalIncludeFile=all_res.hpp
 
 if [ ! -d c ]
 then
@@ -10,6 +11,16 @@ fi
 for e in $(ls img/)
 do
     filepath="c/${e::-4}.h";
-    xxd -i "img/${e}" > "$filepath";
-    echo "#include \"../res/${filepath}\"" >> all_res.hpp
+    incline="#include \"../res/${filepath}\"";
+
+    if [ ! -e $filepath ]
+    then
+        echo "[+] Packing img/$e --> $filepath";
+        xxd -i "img/${e}" > "$filepath";
+        if grep -Fxq "$incline" $FinalIncludeFile; then
+            echo "[!] $incline alredy in $FinalIncludeFile";
+        else
+            echo $incline >> all_res.hpp
+        fi
+    fi
 done
